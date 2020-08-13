a75390a0fc2afd56ab2079de35d88b202d5f3507
Doron Shem Tov <doronst5@gmail.com>
2020-08-13 22:32:52 +0300

    make predictions according to metadata, predict deltaFvc, using week_delta_from_initial_fvc_measure as well as Fvc_initial, sex, age, smokingStatus, week_delta_of_initial_fvc_from_ct

--- osic_pulmonary_fibrosis/pulmonary_fibrosis_analysis.py
+++ osic_pulmonary_fibrosis/pulmonary_fibrosis_analysis.py
@@ -1,20 +1,20 @@
 import numpy as np
 from time import time
 import shutil
-from skimage import exposure
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
-import pydicom
+from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import pearsonr
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 
 
-train_meta = pd.read_csv('input/osic-pulmonary-fibrosis-progression/train.csv')
-test_meta = pd.read_csv('input/osic-pulmonary-fibrosis-progression/test.csv')
+####################
+# meta-data analysis
+####################
 
 def group_base_and_secondary_measurements(train_meta):
     base_measure = train_meta.groupby(by='Patient').first()
@@ -30,41 +30,120 @@ def group_base_and_secondary_measurements(train_meta):
     base_and_secondary_pairs = base_measure.merge(secondary_measures, how='inner', on=['Patient', 'Sex', 'Age', 'SmokingStatus'])
     base_and_secondary_pairs['WeeksDiff'] = base_and_secondary_pairs['Weeks_y'] - base_and_secondary_pairs['Weeks_x']
     base_and_secondary_pairs['FVCDiff'] = base_and_secondary_pairs['FVC_y'] - base_and_secondary_pairs['FVC_x']
+    
+    base_and_secondary_pairs = base_and_secondary_pairs.set_index(['Patient', 'Weeks_y'])
+    base_and_secondary_pairs = base_and_secondary_pairs.drop(['Time'], axis=1)
+    base_and_secondary_pairs.columns = ['Weeks', 'FVC', 'Percent', 'Age', 'Sex', 'SmokingStatus', 'FVCTarget', 'PercentTarget', 'WeeksDiff', 'FVCDiff']
     return base_and_secondary_pairs
 
 
-def plot_weeks_diff_to_fvc(base_and_secondary_pairs):
+def plot_single_var_to_fvc_diff(x, y, column_name):
+    label = 'FVCDiff'
+    feature = x[column_name]
     fig  = plt.figure()
-    plt.scatter(base_and_secondary_pairs['WeeksDiff'], base_and_secondary_pairs['FVCDiff'], marker='o', alpha=0.2)
-
-    corr = pearsonr(base_and_secondary_pairs['WeeksDiff'], base_and_secondary_pairs['FVCDiff'])
-    print(f'pearsonr = {corr}')
-    
-    m, b = np.polyfit(base_and_secondary_pairs['WeeksDiff'], base_and_secondary_pairs['FVCDiff'], 1)
-    plt.plot(base_and_secondary_pairs['WeeksDiff'], m * base_and_secondary_pairs['WeeksDiff'] + b, color='red')
+    plt.scatter(feature, y, marker='o', alpha=0.2)
+    m, b = np.polyfit(feature, y, 1)
+    plt.plot(feature, m * feature + b, color='red')
+    plt.xlabel(column_name)
+    plt.ylabel(label)
     fig.show()
+    # Fit and summarize OLS model on single param
+    reg = sm.OLS(y, feature)
+    print(reg.fit().summary())
+
+
 
 def encode_categories(df):
     label_encoder = LabelEncoder()
     df['SmokingStatus'] = label_encoder.fit_transform(df['SmokingStatus'])
     df['Sex'] = (df['Sex'] == 'Male') + 0
+    return df, label_encoder
+
+def define_features(df):
+    # drop label, columns that will not be available in test-set (leaking)
+    train_x = df.drop(['FVCDiff', 'FVCTarget', 'PercentTarget'], axis=1)
+    # drop non-informative and collinear variable
+    train_x = train_x.drop(['Percent'], axis=1)
+    return train_x
+
+def scale_features(df, scaler=None):    
+    if scaler is None:
+        scaler = MinMaxScaler() 
+        df_scaled = pd.DataFrame(scaler.fit_transform(df), 
+                                                columns=df.columns, 
+                                                index=df.index)
+    else:
+        df_scaled = pd.DataFrame(scaler.transform(df), 
+                                                columns=df.columns, 
+                                                index=df.index)
+    return df_scaled, scaler
+
+def make_test_data_to_model_compatible(test_meta):
+    possible_weeks = pd.DataFrame(np.arange(-12,133), columns=['PossibleWeeks'])    
+    possible_weeks['cross_product_key'] = 1
+    test_meta['cross_product_key'] = 1
+    df = test_meta.merge(possible_weeks, on='cross_product_key')
+    df['WeeksDiff'] = df['PossibleWeeks'] - df['Weeks']
+    df = df[(df['WeeksDiff'] >= -12) & (df['WeeksDiff'] <= 133)]
+    df = df.drop('cross_product_key', axis=1)
     return df
 
-base_and_secondary_pairs = group_base_and_secondary_measurements(train_meta)
-base_and_secondary_pairs = base_and_secondary_pairs.set_index(['Patient', 'Weeks_y'])
-plot_weeks_diff_to_fvc(base_and_secondary_pairs)    
+def analyse_each_variable(train_x, train_y):
+    plot_single_var_to_fvc_diff(train_x, train_y, 'WeeksDiff')    
+    plot_single_var_to_fvc_diff(train_x, train_y, 'FVC')
+    plot_single_var_to_fvc_diff(train_x, train_y, 'Weeks')    
+    plot_single_var_to_fvc_diff(train_x, train_y, 'Age')    
+    plot_single_var_to_fvc_diff(train_x, train_y, 'Sex')    
+    plot_single_var_to_fvc_diff(train_x, train_y, 'SmokingStatus')    
+    
+    
+# Load meta-data
+train_meta = pd.read_csv('input/osic-pulmonary-fibrosis-progression/train.csv')
+test_meta = pd.read_csv('input/osic-pulmonary-fibrosis-progression/test.csv')
 
-base_and_secondary_pairs_enc =  encode_categories(base_and_secondary_pairs)
+base_and_secondary_pairs = group_base_and_secondary_measurements(train_meta)
+base_and_secondary_pairs_enc, label_encoder = encode_categories(base_and_secondary_pairs)
 
 train_y = base_and_secondary_pairs_enc['FVCDiff']
-train_x = base_and_secondary_pairs_enc.drop(['FVCDiff'], axis=1)
+train_x = define_features(base_and_secondary_pairs_enc)
+analyse_each_variable(train_x, train_y)
+train_x_scaled, scaler = scale_features(train_x)
 
 
-reg = LinearRegression().fit(train_x, train_y)
-print(reg.score(train_x, train_y))
-print(train_x.columns)
-print(reg.coef_)
-print(reg)
+# Fit and summarize OLS model
+ols_model = sm.OLS(train_y, train_x_scaled)
+predictor = ols_model.fit()
+print(predictor.summary())
+
+
+plt.xlabel('actual FVC at secondary checkup')
+plt.ylabel('predicted FVC at secondary checkup')
+plt.scatter(base_and_secondary_pairs['FVCTarget'], train_x['FVC'] + predictor.predict(train_x_scaled), alpha=0.2)
+plt.scatter(base_and_secondary_pairs['FVC'], base_and_secondary_pairs['FVC'], alpha=0.2, color='red')
+plt.show()
+
+
+test_x = make_test_data_to_model_compatible(test_meta)
+test_x['SmokingStatus'] = label_encoder.transform(test_x['SmokingStatus'])
+test_x['Sex'] = (test_x['Sex'] == 'Male') + 0
+
+test_x_dont_scale = test_x[['Patient', 'PossibleWeeks']]
+patient_week = pd.Series(test_x_dont_scale['Patient'] + '_' 
+                         + test_x_dont_scale['PossibleWeeks'].astype(str), 
+                         name='Patient_Week')
+test_x = test_x.drop(['Patient', 'PossibleWeeks', 'Percent'], axis=1)
+test_x_scaled, _ = scale_features(test_x, scaler)
+fvc_diff_prediction = predictor.predict(test_x_scaled)
+pd.concat([patient_week, test_x, (test_x['FVC'] + fvc_diff_prediction)], axis=1)
+train_x_scaled.shape
+test_x_scaled.shape
+
+###################
+# CT scans analysis
+###################
+from skimage import exposure
+import pydicom
+
 def even_select(array, m):
     """
     select m elements from array, trying to pick them as evenly as possible
